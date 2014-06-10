4f5818ac68d807173acbb3a21fdb432c457e394f
Ben Wu <wubin28@gmail.com>
2014-06-10 22:49:59 +0800

    Created a blank Java project using Maven.

---
+++ td/src/main/java/kata/td/App.java
@@ -0,0 +1,13 @@
+package kata.td;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
