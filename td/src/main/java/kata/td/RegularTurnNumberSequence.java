3b52edfd0e54fb405b939be736e84b8eb73f3309
Ben Wu <wubin28@gmail.com>
2014-06-16 21:29:15 +0800

    Created constructor RegularTurnNumberSequence(int).

--- td/src/main/java/kata/td/RegularTurnNumberSequence.java
+++ td/src/main/java/kata/td/RegularTurnNumberSequence.java
@@ -4,4 +4,7 @@ package kata.td;
  * Created by benwu on 14-6-16.
  */
 public class RegularTurnNumberSequence extends NumberSequence {
+    public RegularTurnNumberSequence(int startingTurnNumber) {
+        super.turnNumber = startingTurnNumber;
+    }
 }
