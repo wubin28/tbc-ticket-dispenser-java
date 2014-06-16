9125a66ccee6468fc7e0a0a9c91a422bec755944
Ben Wu <wubin28@gmail.com>
2014-06-16 20:15:29 +0800

    Working on TODO: Change the interface NumberSequence to be a class to remove the duplicated code in its subclasses.

--- td/src/main/java/kata/td/NumberSequence.java
+++ td/src/main/java/kata/td/NumberSequence.java
@@ -4,5 +4,6 @@ package kata.td;
  * Created by benwu on 14-6-16.
  */
 public interface NumberSequence {
+    // TODO-working-on: Change the interface NumberSequence to be a class to remove the duplicated code in its subclasses
     int getNextTurnNumber();
 }
