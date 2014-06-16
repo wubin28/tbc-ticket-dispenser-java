0f678e0ee75459f608a1119af3abbcf9d84648e7
Ben Wu <wubin28@gmail.com>
2014-06-16 18:35:27 +0800

    Created method FakeTurnNumberSequence.setExpectedNextTurnNumber().

--- td/src/main/java/kata/td/FakeTurnNumberSequence.java
+++ td/src/main/java/kata/td/FakeTurnNumberSequence.java
@@ -4,4 +4,7 @@ package kata.td;
  * Created by benwu on 14-6-16.
  */
 public class FakeTurnNumberSequence implements NumberSequence {
+    public void setExpectedNextTurnNumber(int expectedNextTurnNumber) {
+
+    }
 }
