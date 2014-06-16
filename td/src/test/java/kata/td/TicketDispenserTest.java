9fe9c4e380d43ad435d5aca801e4f4fbb928c592
Ben Wu <wubin28@gmail.com>
2014-06-16 11:57:06 +0800

    Working on TODO: a new ticket should have the turn number subsequent to the previous ticket. Wrote an assertion for its test.

--- td/src/test/java/kata/td/TicketDispenserTest.java
+++ td/src/test/java/kata/td/TicketDispenserTest.java
@@ -13,7 +13,13 @@ public class TicketDispenserTest {
         assertEquals(5, 2 + 3);
     }
 
-    // TODO-acceptance-test: a new ticket should have the turn number subsequent to the previous ticket
+    // TODO-acceptance-test-working-on: a new ticket should have the turn number subsequent to the previous ticket
+    @Test
+    public void a_new_ticket_should_have_the_turn_number_subsequent_to_the_previous_ticket() {
+        // Assert
+        assertEquals(1, newTicket.getTurnNumber() - previousTicket.getTurnNumber());
+    }
+
     // TODO-acceptance-test: a new ticket should have the turn number subsequent to the previous ticket from another dispenser
 
     // TODO-unit-test: the class TicketDispenser should dispense the ticket number 11 if give a turn number 11 to it
