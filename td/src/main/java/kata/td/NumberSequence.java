ef85e1450719369a48d3868e948d13f6f0893955
Ben Wu <wubin28@gmail.com>
2014-06-16 21:51:28 +0800

    Created constructor NumberSequence(int) and default constructor NumberSequence().

--- td/src/main/java/kata/td/NumberSequence.java
+++ td/src/main/java/kata/td/NumberSequence.java
@@ -6,6 +6,13 @@ package kata.td;
 public class NumberSequence {
     protected static int turnNumber = 0;
 
+    public NumberSequence() {
+        this(0);
+    }
+    public NumberSequence(int startingTurnNumber) {
+        turnNumber = startingTurnNumber;
+    }
+
     // TODO-working-on: create a constructor NumberSequence(int) to eliminate the duplicated code in the constructors of its subclass VipTurnNumberSequence and RegularTurnNumberSequence
 
     public int getNextTurnNumber()
