38636ad81469027299ce6b5769a42f6e0d181644
Ben Wu <wubin28@gmail.com>
2014-06-16 19:28:07 +0800

    Implemented method getNextTurnNumber() in class FakeTurnNumberSequence.

--- td/src/main/java/kata/td/FakeTurnNumberSequence.java
+++ td/src/main/java/kata/td/FakeTurnNumberSequence.java
@@ -5,14 +5,20 @@ package kata.td;
  */
 public class FakeTurnNumberSequence implements NumberSequence {
     private int callsCount = 0;
+    private int expectedNextTurnNumber;
 
     public void setExpectedNextTurnNumber(int expectedNextTurnNumber) {
-
+        this.expectedNextTurnNumber = expectedNextTurnNumber;
     }
 
     public void verifyMethodGetNextTurnNumberWasCalledOnce() {
         if (this.callsCount != 1) {
             throw new IllegalStateException("The method getNextTurnNumber() should be called once.");
         }
+    }
+
+    @Override
+    public int getNextTurnNumber() {
+        return this.expectedNextTurnNumber;
     }
 }
