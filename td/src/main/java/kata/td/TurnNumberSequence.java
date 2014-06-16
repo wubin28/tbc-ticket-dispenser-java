3146754699ca6c95d0bf696e51e2461c998eab6a
Ben Wu <wubin28@gmail.com>
2014-06-16 18:41:24 +0800

    Made class TurnNumberSequence implement the interface NumberSequence.

--- td/src/main/java/kata/td/TurnNumberSequence.java
+++ td/src/main/java/kata/td/TurnNumberSequence.java
@@ -3,7 +3,7 @@ package kata.td;
 /**
  * Created by benwu on 14-6-10.
  */
-public class TurnNumberSequence {
+public class TurnNumberSequence implements NumberSequence {
     private static int _turnNumber = 0;
 
     public static int getNextTurnNumber()
