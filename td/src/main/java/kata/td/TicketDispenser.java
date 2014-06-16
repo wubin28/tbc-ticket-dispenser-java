b84d1e4059bf6be8c29fc2dd7ed13064ae07e053
Ben Wu <wubin28@gmail.com>
2014-06-16 18:36:54 +0800

    Created constructor TicketDispenser(NumberSequence).

--- td/src/main/java/kata/td/TicketDispenser.java
+++ td/src/main/java/kata/td/TicketDispenser.java
@@ -4,6 +4,10 @@ package kata.td;
  * Created by benwu on 14-6-10.
  */
 public class TicketDispenser {
+    public TicketDispenser(NumberSequence numberSequence) {
+        
+    }
+
     public TurnTicket getTurnTicket()
     {
         // TODO: Depending on a static method violates the Dependency Inversion Principle and Open-Closed Principle.
