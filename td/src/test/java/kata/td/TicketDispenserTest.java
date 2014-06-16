5ac1818ead8aae448e737be883b4937c3124b43d
Ben Wu <wubin28@gmail.com>
2014-06-16 19:38:50 +0800

    Working on TODO: the turn number sequence of the vip customers starts from 1001. Wrote an assertion for its test.

--- td/src/test/java/kata/td/TicketDispenserTest.java
+++ td/src/test/java/kata/td/TicketDispenserTest.java
@@ -50,6 +50,12 @@ public class TicketDispenserTest {
         fakeTurnNumberSequence.verifyMethodGetNextTurnNumberWasCalledOnce();
     }
 
-    // TODO-new-feature: the turn number sequence of the vip customers starts from 1001
+    // TODO-new-feature-working-on: the turn number sequence of the vip customers starts from 1001
+    @Test
+    public void the_turn_number_sequence_of_the_vip_customers_starts_from_1001() {
+        // Assert
+        assertEquals(1001, ticket.getTurnNumber());
+    }
+
     // TODO-new-feature: the turn number sequence of the regular customers starts from 2001
 }
