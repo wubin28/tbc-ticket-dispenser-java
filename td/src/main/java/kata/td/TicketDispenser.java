dbb0b06756a9418ae3a170d338004b25011bb2c9
Ben Wu <wubin28@gmail.com>
2014-06-16 19:19:44 +0800

    Wrote the intention code for making the class TicketDispenser depend on an abstraction -- the interface NumberSequence.

--- td/src/main/java/kata/td/TicketDispenser.java
+++ td/src/main/java/kata/td/TicketDispenser.java
@@ -4,18 +4,20 @@ package kata.td;
  * Created by benwu on 14-6-10.
  */
 public class TicketDispenser {
+    private NumberSequence numberSequence;
+
     public TicketDispenser() {
         this(new TurnNumberSequence());
     }
 
     public TicketDispenser(NumberSequence numberSequence) {
-
+        this.numberSequence = numberSequence;
     }
 
     public TurnTicket getTurnTicket()
     {
         // TODO-working-on: Depending on a static method violates the Dependency Inversion Principle and Open-Closed Principle.
-        int newTurnNumber = TurnNumberSequence.getNextTurnNumber();
+        int newTurnNumber = this.numberSequence.getNextTurnNumber();
         TurnTicket newTurnTicket = new TurnTicket(newTurnNumber);
 
         return newTurnTicket;
