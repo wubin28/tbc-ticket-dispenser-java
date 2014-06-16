c94d4cefc2b4ca82a0a4ae984e98b2b4a6901a7f
Ben Wu <wubin28@gmail.com>
2014-06-16 19:26:26 +0800

    Created and implemented method FakeTurnNumberSequence.verifyMethodGetNextTurnNumberWasCalledOnce().

--- td/src/main/java/kata/td/FakeTurnNumberSequence.java
+++ td/src/main/java/kata/td/FakeTurnNumberSequence.java
@@ -4,7 +4,15 @@ package kata.td;
  * Created by benwu on 14-6-16.
  */
 public class FakeTurnNumberSequence implements NumberSequence {
+    private int callsCount = 0;
+
     public void setExpectedNextTurnNumber(int expectedNextTurnNumber) {
 
+    }
+
+    public void verifyMethodGetNextTurnNumberWasCalledOnce() {
+        if (this.callsCount != 1) {
+            throw new IllegalStateException("The method getNextTurnNumber() should be called once.");
+        }
     }
 }
