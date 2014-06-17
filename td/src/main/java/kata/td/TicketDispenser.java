13e4589e294ad8c284037c3865e00314f846d18c
Ben Wu <wubin28@gmail.com>
2014-06-17 11:03:39 +0800

    Implemented constructor TicketDispenser(TurnNumberSequence).

--- td/src/main/java/kata/td/TicketDispenser.java
+++ td/src/main/java/kata/td/TicketDispenser.java
@@ -5,12 +5,14 @@ package kata.td;
  */
 public class TicketDispenser {
 
+    private TurnNumberSequence turnNumberSequence;
+
     public TicketDispenser() {
         this(new TurnNumberSequence());
     }
 
     public TicketDispenser(TurnNumberSequence turnNumberSequence) {
-
+        this.turnNumberSequence = turnNumberSequence;
     }
 
     public TurnTicket getTurnTicket()
