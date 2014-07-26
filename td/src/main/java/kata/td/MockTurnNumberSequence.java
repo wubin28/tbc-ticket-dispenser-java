c7194472b364436e88fd25c82bed5d05049693ed
Ben Wu <wubin28@gmail.com>
2014-07-26 12:49:42 +0800

    Fixed compiler error: Made class MockTurnNumberSequence extend class TurnNumberSequence.

--- td/src/main/java/kata/td/MockTurnNumberSequence.java
+++ td/src/main/java/kata/td/MockTurnNumberSequence.java
@@ -3,7 +3,7 @@ package kata.td;
 /**
  * Created by benwu on 14-7-26.
  */
-public class MockTurnNumberSequence {
+public class MockTurnNumberSequence extends TurnNumberSequence {
     public void arrangeNextTurnNumber(int nextTurnNumber) {
 
     }
