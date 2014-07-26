7d5b2e948462840452ac7503e918a6eb6c1bc4d2
Ben Wu <wubin28@gmail.com>
2014-07-26 16:24:29 +0800

    Fixed compiler error: Created and implemented constructor TurnNumberSequence(int).

--- td/src/main/java/kata/td/TurnNumberSequence.java
+++ td/src/main/java/kata/td/TurnNumberSequence.java
@@ -6,6 +6,10 @@ package kata.td;
 public class TurnNumberSequence {
     private static int _turnNumber = 0;
 
+    public TurnNumberSequence(int firstNumber) {
+        this._turnNumber = firstNumber;
+    }
+
     public int getNextTurnNumber()
     {
         return _turnNumber++;
