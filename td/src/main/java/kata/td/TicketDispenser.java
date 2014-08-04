30b82aa5a1b9c7ffa2965bc8007376f385d81e4e
Ben Wu <wubin28@gmail.com>
2014-08-04 10:28:23 +0800

    Fixed compiler error: Used an object to call a non-static member method.

--- td/src/main/java/kata/td/TicketDispenser.java
+++ td/src/main/java/kata/td/TicketDispenser.java
@@ -4,6 +4,8 @@ package kata.td;
  * Created by benwu on 14-6-10.
  */
 public class TicketDispenser {
+    private TurnNumberSequence turnNumberSequence;
+
     public TicketDispenser(TurnNumberSequence turnNumberSequence) {
 
     }
@@ -15,7 +17,7 @@ public class TicketDispenser {
     public TurnTicket getTurnTicket()
     {
         // TODO: Depending on a static method violates the Dependency Inversion Principle and Open-Closed Principle.
-        int newTurnNumber = TurnNumberSequence.getNextTurnNumber();
+        int newTurnNumber = turnNumberSequence.getNextTurnNumber();
         TurnTicket newTurnTicket = new TurnTicket(newTurnNumber);
 
         return newTurnTicket;
