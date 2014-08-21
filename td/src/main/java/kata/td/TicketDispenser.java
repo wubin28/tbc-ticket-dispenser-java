fe4175ef227f85dd3976bc389e8811222b644fb4
Ben Wu <wubin28@gmail.com>
2014-08-21 21:57:09 +0800

    Fixing test run error: Implemented constructor TicketDispenser(TurnNumberSequence) and TicketDispenser().

--- td/src/main/java/kata/td/TicketDispenser.java
+++ td/src/main/java/kata/td/TicketDispenser.java
@@ -7,11 +7,11 @@ public class TicketDispenser {
     private TurnNumberSequence turnNumberSequence;
 
     public TicketDispenser(TurnNumberSequence turnNumberSequence) { // Enabling Point
-
+        this.turnNumberSequence = turnNumberSequence;
     }
 
     public TicketDispenser() {
-
+        this(new TurnNumberSequence());
     }
 
     public TurnTicket getTurnTicket()
