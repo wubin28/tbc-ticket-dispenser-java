fb8443dd623cd009a5bfbeac45b4157600756168
Ben Wu <wubin28@gmail.com>
2014-08-21 18:59:28 +0800

    Finished TODO-characterization: a new ticket should have the turn number subsequent to the previous ticket.

--- td/src/test/java/kata/td/TicketDispenserTest.java
+++ td/src/test/java/kata/td/TicketDispenserTest.java
@@ -9,9 +9,15 @@ import static org.junit.Assert.assertEquals;
  */
 public class TicketDispenserTest {
 
-    // TODO-characterization-working-on: a new ticket should have the turn number subsequent to the previous ticket
     @Test
     public void a_new_ticket_should_have_the_turn_number_subsequent_to_the_previous_ticket() {
+        // Arrange
+        TicketDispenser ticketDispenser = new TicketDispenser();
+
+        // Act
+        TurnTicket previousTicket = ticketDispenser.getTurnTicket();
+        TurnTicket newTicket = ticketDispenser.getTurnTicket();
+
         // Assert
         assertEquals(1, newTicket.getTurnNumber() - previousTicket.getTurnNumber());
     }
